0ab935af8721d3bfac7e5a415e6713886553ba5f
Ondřej Tůma <ondr@centrum.cz>
2019-03-08 20:00:38 +0100

    claim heading and date svg animated

--- components/Claim/index.jsx
+++ components/Claim/index.jsx
@@ -1,24 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {useGlobal} from 'reactn';
 import classNames from 'classnames';
 
 import Slider from 'Components/Slider';
+import ClaimSVG from 'Svg/marketa_dominik_animated.svg';
+import DateSVG from 'Svg/date_animated.svg';
 import WreathSvg from 'Svg/wreath_animated.svg';
 
 import slides from 'Consts/slides';
 
 import styles from './styles.scss';
-import Transition from "../Transition";
 
 function Claim({date, heading}) {
     const [isSliderLight] = useGlobal('isSliderLight');
-    const [isLoaded, setIsLoaded] = useState(false);
 
     date = new Date(date);
-
-    useEffect(() => {
-        setIsLoaded(true);
-    }, []);
 
     return (
         <div className={styles.wrapper}>
@@ -27,14 +23,12 @@ function Claim({date, heading}) {
                 [styles.light]: isSliderLight,
             })}>
                 <WreathSvg className={classNames(styles.wreath, styles.up)}/>
-                <Transition name={'slideLeft'}>
-                    {isLoaded && (
-                        <div>
-                            <h1 className={styles.heading}>{heading}</h1>
-                            <p className={styles.date}>{date.getDate()}. {date.getMonth() + 1}. {date.getFullYear()}</p>
-                        </div>
-                    )}
-                </Transition>
+                <ClaimSVG className={styles.claimSvg}/>
+                <DateSVG className={styles.dateSvg}/>
+                <div style={{display: 'none'}}>
+                    <h1 className={styles.heading}>{heading}</h1>
+                    <p className={styles.date}>{date.getDate()}. {date.getMonth() + 1}. {date.getFullYear()}</p>
+                </div>
                 <WreathSvg className={classNames(styles.wreath, styles.down)}/>
             </div>
         </div>
