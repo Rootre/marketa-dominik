396f3dad95c8c0b3c8993b8e33610620ea0c1d8c
Ondřej Tůma <ondr@centrum.cz>
2019-03-08 14:23:21 +0100

    time in bit component

--- components/Bit/index.jsx
+++ components/Bit/index.jsx
@@ -6,13 +6,16 @@ import GoogleMap from 'Components/GoogleMap';
 
 import styles from './styles.scss';
 
-function Bit({even, image, map, text, title}) {
+function Bit({even, image, map, text, time, title}) {
     return (
         <div className={classNames(styles.wrapper, {
             [styles.even]: even,
         })}>
             <div className={styles.content}>
-                <h3 className={styles.heading}>{title}</h3>
+                <h3 className={styles.heading}>
+                    {title}
+                    {time && <span className={styles.time}>{time}</span>}
+                </h3>
                 {text && <div className={styles.text} dangerouslySetInnerHTML={{__html: text}}/>}
                 {map && <GoogleMap className={styles.map} {...map}/>}
             </div>
