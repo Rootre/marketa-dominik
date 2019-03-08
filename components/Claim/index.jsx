290d30adcb316246589c02d8798078f256487d11
Ondřej Tůma <ondr@centrum.cz>
2019-03-08 19:20:02 +0100

    animated wreath

--- components/Claim/index.jsx
+++ components/Claim/index.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useGlobal} from 'reactn';
 import classNames from 'classnames';
 
 import Slider from 'Components/Slider';
-import WreathSvg from 'Svg/wreath.svg';
+import WreathSvg from 'Svg/wreath_animated.svg';
 
 import slides from 'Consts/slides';
 
 import styles from './styles.scss';
+import Transition from "../Transition";
 
 function Claim({date, heading}) {
     const [isSliderLight] = useGlobal('isSliderLight');
+    const [isLoaded, setIsLoaded] = useState(false);
+
     date = new Date(date);
+
+    useEffect(() => {
+        setIsLoaded(true);
+    }, []);
 
     return (
         <div className={styles.wrapper}>
@@ -20,8 +27,14 @@ function Claim({date, heading}) {
                 [styles.light]: isSliderLight,
             })}>
                 <WreathSvg className={classNames(styles.wreath, styles.up)}/>
-                <h1 className={styles.heading}>{heading}</h1>
-                <p className={styles.date}>{date.getDate()}. {date.getMonth() + 1}. {date.getFullYear()}</p>
+                <Transition name={'slideLeft'}>
+                    {isLoaded && (
+                        <div>
+                            <h1 className={styles.heading}>{heading}</h1>
+                            <p className={styles.date}>{date.getDate()}. {date.getMonth() + 1}. {date.getFullYear()}</p>
+                        </div>
+                    )}
+                </Transition>
                 <WreathSvg className={classNames(styles.wreath, styles.down)}/>
             </div>
         </div>
