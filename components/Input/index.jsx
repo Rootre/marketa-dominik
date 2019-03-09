5feb8702cfc4f0c666e5e9f03706bc8480460e23
Ondřej Tůma <ondr@centrum.cz>
2019-03-09 03:50:24 +0100

    form input better is filled recognition

--- components/Input/index.jsx
+++ components/Input/index.jsx
@@ -1,4 +1,4 @@
-import React, {forwardRef, useImperativeHandle, useEffect, useState} from 'react';
+import React, {forwardRef, useImperativeHandle, useState} from 'react';
 import classNames from 'classnames';
 
 import InputHidden from '../InputHidden';
@@ -11,7 +11,6 @@ import styles from './styles.scss';
 function Input({className, errorMessage, id = generateID(), label, name, onChange, type = 'text', value = '', ...rest}, ref) {
     const [stateValue, setStateValue] = useState('');
     const [isFocused, setIsFocused] = useState(false);
-    const [hasError] = useState(false);
 
     if (type === 'hidden') {
         return <InputHidden name={name} value={value}/>;
@@ -27,8 +26,8 @@ function Input({className, errorMessage, id = generateID(), label, name, onChang
     return (
         <div className={classNames(styles.container, className, {
             [styles.focused]: isFocused,
-            [styles.filled]: onChange ? value && value.length > 0 : stateValue && stateValue.length > 0,
-            [styles.hasError]: hasError,
+            [styles.filled]: onChange ? value !== '' : stateValue !== '',
+            [styles.hasError]: errorMessage,
         })}>
             {label && <label className={styles.label} htmlFor={id}>{label}</label>}
             {type === 'password' ? (
@@ -44,7 +43,7 @@ function Input({className, errorMessage, id = generateID(), label, name, onChang
                        {...rest}
                 />
             )}
-            {hasError && <p className={styles.error}>{errorMessage}</p>}
+            {errorMessage && <p className={styles.error}>{errorMessage}</p>}
         </div>
     )
 }
